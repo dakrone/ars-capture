bbc0c71437cff8c476aa900ffde66b4d2f033b42
Lee Hinman <lee@writequit.org>
2017-11-09 17:37:33 -0700

    Fix mapping

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject ars-capture "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Capture adaptive replica stats at a fixed rate"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
--- src/ars_capture/core.clj
+++ src/ars_capture/core.clj
@@ -1,5 +1,6 @@
 (ns ars-capture.core
-  (:require [clj-http.client :as http]
+  (:require [clojure.string :as str]
+            [clj-http.client :as http]
             [cheshire.core :as json]
             [carica.core :refer :all])
   (:import (java.util.concurrent ScheduledThreadPoolExecutor
@@ -14,20 +15,30 @@
   []
   (try
     (let [time (System/currentTimeMillis)
-          nodes-stats (:body (http/get
-                              (str (config :es :url) "/_nodes/stats")
-                              {:headers (merge {"Content-Type" "application/json"}
-                                               (config :es :headers))
-                               :as :json}))
-          doc {:time time
-               :stats nodes-stats}]
+          nodes-stats (-> (http/get
+                           (str (config :es :url) "/_nodes/stats")
+                           {:headers (merge {"Content-Type" "application/json"}
+                                            (config :es :headers))
+                            :as :json})
+                          :body
+                          :nodes)
+          nodes (keys nodes-stats)
+          docs (for [node nodes]
+                 {:timestamp time
+                  :node node
+                  :stats (-> nodes-stats node :adaptive_selection)})]
       (future
         (try
           (let [url (str (config :es-store :url) "/"
-                         (config :es-store :index) "/doc/")]
-            (when debug (println "putting" doc "at" url))
+                         (config :es-store :index) "/doc/_bulk")
+                bulk-body (str/join
+                           "\n"
+                           (for [doc docs]
+                             (str (json/encode {:index {}}) "\n"
+                                  (json/encode doc) "\n")))]
+            (when debug (println "putting" bulk-body "at" url))
             (http/post url
-                       {:body (json/encode doc)
+                       {:body bulk-body
                         :headers (merge {"Content-Type" "application/json"}
                                         (config :es :headers))}))
           (print ".")
@@ -46,12 +57,23 @@
   "Main entry point"
   [& args]
   (println "Starting up...")
-  (let [threadpool (ScheduledThreadPoolExecutor. (config :threads))
-        block (promise)]
+  (if (http/missing? (http/head (str (config :es-store :url) "/"
+                                     (config :es-store :index))
+                                {:throw-exceptions false}))
+    (do
+      (print "Creating" (config :es-store :index) "index...")
+      (-> (http/put (str (config :es-store :url) "/" (config :es-store :index))
+                  {:body (json/encode (config :es-store :creation-settings))
+                   :headers (merge {"Content-Type" "application/json"}
+                                   (config :es :headers))})
+          :status
+          println))
+    (println "Index" (config :es-store :index) "already exists"))
+  (let [threadpool (ScheduledThreadPoolExecutor. (config :threads))]
     (.scheduleAtFixedRate threadpool retrieve-and-store-stats
-                          1 ;; initial delay of 1 second
+                          1 ;; initial delay of 1 second, just because
                           (config :period-seconds)
                           TimeUnit/SECONDS)
     (println "Reading from" (config :es :url) "and writing to" (config :es-store :url))
     ;; Block forever - TODO: make this not suck
-    @block))
+    @(promise)))
